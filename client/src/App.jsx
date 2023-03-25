e9187143e4715f8c892d73706ad350c081688e3c
Rohinish <singhrohinish09@gmail.com>
2023-03-25 18:23:08 +0530

    api calls

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import HomePage from "./components/homePage/HomePage";
-
+import Login from "./components/login/Login";
+import SignUp from "./components/signup/SignUp";
 function App() {
   return (
     <div>
-      <HomePage />
+     <HomePage />
     </div>
   );
 }
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,28 +1,70 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
 function Login() {
-    return (
-      <div className="bg-white lg:w-4/12 md:6/12 w-10/12 m-auto my-10 shadow-md">
-        <div className="py-8 px-8 rounded-xl">
-          <h1 className="font-medium text-2xl mt-3 text-center">Login</h1>
-          <form action="" className="mt-6">
-            <div className="my-5 text-sm">
-              <label htmlFor="username" className="block text-black">Email</label>
-              <input type="text" autoFocus id="Email" className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full" placeholder="Email" />
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = { email, password };
+    try {
+      const response = await axios.post('/api/login', data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="bg-white lg:w-4/12 md:6/12 w-10/12 m-auto my-10 shadow-md">
+      <div className="py-8 px-8 rounded-xl">
+        <h1 className="font-medium text-2xl mt-3 text-center">Login</h1>
+        <form onSubmit={handleSubmit} className="mt-6">
+          <div className="my-5 text-sm">
+            <label htmlFor="email" className="block text-black">
+              Email
+            </label>
+            <input
+              type="text"
+              autoFocus
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full"
+              placeholder="Email"
+            />
+          </div>
+          <div className="my-5 text-sm">
+            <label htmlFor="password" className="block text-black">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full"
+              placeholder="Password"
+            />
+            <div className="flex justify-end mt-2 text-xs text-gray-600">
+              <a href="#">Forget Password?</a>
             </div>
-            <div className="my-5 text-sm">
-              <label htmlFor="password" className="block text-black">Password</label>
-              <input type="password" id="password" className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full" placeholder="Password" />
-              <div className="flex justify-end mt-2 text-xs text-gray-600">
-                <a href="#">Forget Password?</a>
-              </div>
-            </div>
-  
-            <button className="block text-center text-white bg-gray-800 p-3 duration-300 rounded-sm hover:bg-black w-full">Login</button>
-          </form>  
-          <p className="mt-12 text-xs text-center font-light text-gray-400">Don't have an account? <a href="#" className="text-black font-medium">Create One</a></p>
-  
-        </div>
+          </div>
+
+          <button className="block text-center text-white bg-gray-800 p-3 duration-300 rounded-sm hover:bg-black w-full">
+            Login
+          </button>
+        </form>
+        <p className="mt-12 text-xs text-center font-light text-gray-400">
+          Don't have an account?{' '}
+          <a href="#" className="text-black font-medium">
+            Create One
+          </a>
+        </p>
       </div>
-    )
-  }
-  
-  export default Login
+    </div>
+  );
+}
+
+export default Login;
