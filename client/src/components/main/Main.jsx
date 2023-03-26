d754a0145371527dd7e5053bf0fb38ce6c0ec4c4
Rohinish <singhrohinish09@gmail.com>
2023-03-26 11:07:51 +0530

    added profile sliding in navbar

--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -1,7 +1,7 @@
 import Product from "../products/Product";
 function Main() {
   return (
-    <div className="grid grid-cols-3 overflow-y-auto w-[120%] ml-20">
+    <div className="mt-10 grid grid-cols-3 overflow-y-auto w-[120%] ml-20">
       <Product />
       <Product />
       <Product />
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,15 @@
+import { useContext } from "react"
+import { MyContext } from "../../contexts/MyContextProvider"
 function Navbar() {
+  const {clickonprofile,
+    clickprofile} = useContext(MyContext)
   return (
-    <div className='flex border-2 border-black px-7 py-2 justify-between items-center fixed w-full mb-2'>
+    <div className='bg-white flex border-2 border-black px-7 py-2 justify-between items-center fixed w-full mb-2 h-16'>
       <h1>Name</h1>
-      <input type='text' className='border-2 border-black rounded-2xl px-3 py-1'/>
+      {/* <input type='text' className='border-2 border-black rounded-2xl px-3 py-1'/> */}
+      <button onClick={clickprofile}>
+        <img className="h-12 m-12 rounded-full ring" src="profile.png" alt="Profile" />
+        </button>
     </div>
   )
 }
